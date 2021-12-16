205b4d52a450ab61aac69c360cc5505fe66c912c
Osaretin Frank <osaretin.frank10@gmail.com>
2021-12-16 12:20:09 +0100

    toggles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const main = document.querySelector("main");
 const topHeader = document.querySelector(".top");
 const btn = document.querySelector("#search");
 const searchInput = document.querySelector("#search-name");
+const ham = document.querySelector(".ham");
+const search2 = document.querySelector(".search-2");
+const searchHide = document.querySelector(".search-2 a")
 
 function toggleInterfaces(){
     main.classList.remove("hide")
@@ -12,4 +15,14 @@ function toggleInterfaces(){
 
 btn.addEventListener("click", ()=>{
     toggleInterfaces()
+})
+
+ham.addEventListener("click", ()=>{
+    ham.classList.add("hide");
+    search2.classList.remove("hide-2");
+})
+
+searchHide.addEventListener("click", ()=>{
+    ham.classList.remove("hide");
+    search2.classList.add("hide-2");
 })
